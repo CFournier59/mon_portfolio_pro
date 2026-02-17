182341cd9a983987b2ec3cfca124a57dcdb5f7f2
CFournier59 <clementfournier.c@gmail.com>
2026-02-17 15:50:06 +0100

    implémentation du composant Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Intro from './components/Intro'
 import About from './components/About'
+import Projects from './components/Projects'
 
 export default function App() {
    return (
@@ -9,6 +10,7 @@ export default function App() {
          <main>
             <Intro />
             <About />
+            <Projects />
          </main>
       </div>
    )
