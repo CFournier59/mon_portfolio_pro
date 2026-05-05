206756dc7d681c2088e241e1d910933bee4ec768
CFournier59 <clementfournier.c@gmail.com>
2026-05-05 17:34:25 +0200

    reprise du look - Intro

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,95 +13,47 @@ export default function Header() {
    }
 
    return (
-      <header className="fixed top-0 left-0 right-0 z-50 bg-black backdrop-blur-md border-b border-yellow">
+      <header className="fixed top-0 left-0 right-0 z-50 bg-white backdrop-blur-md border-b-2 border-blue">
          <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16">
                <div
-                  className="flex justify between items-center cursor-pointer"
+                  className=" cursor-pointer"
                   onClick={() => scrollToSection('home')}
                >
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     width="24"
-                     height="24"
-                     viewBox="0 0 24 24"
-                     className="size-11"
-                  >
-                     <defs>
-                        <radialGradient
-                           id="myGradient"
-                           cx="50%"
-                           cy="50%"
-                           r="70%"
-                        >
-                           <stop offset="30%" stopColor="rgba(253,101,0,1)" />
-                           <stop offset="30%" stopColor="rgba(0,254,212,1)" />
-                        </radialGradient>
-                     </defs>
-
-                     <path
-                        fill="url(#myGradient)"
-                        d="M15 12c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-.199.02-.393.057-.581 1.474.541 2.927-.882 2.405-2.371.174-.03.354-.048.538-.048 1.657 0 3 1.344 3 3zm-2.985-7c-7.569 0-12.015 6.551-12.015 6.551s4.835 7.449 12.015 7.449c7.733 0 11.985-7.449 11.985-7.449s-4.291-6.551-11.985-6.551zm-.015 12c-2.761 0-5-2.238-5-5 0-2.761 2.239-5 5-5 2.762 0 5 2.239 5 5 0 2.762-2.238 5-5 5z"
-                     />
-                  </svg>
-                  <p className="text-xl font-semibold text-turquoise w-full text-center">
-                     <span className="text-orange">Clément</span> Développe
+                  <p className="squarified text-5xl">
+                     <span className="squarified text-blue">D</span>-vlop
                   </p>
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     width="24"
-                     height="24"
-                     viewBox="0 0 24 24"
-                     className="size-11"
-                  >
-                     <defs>
-                        <radialGradient
-                           id="myGradient"
-                           cx="50%"
-                           cy="50%"
-                           r="70%"
-                        >
-                           <stop offset="30%" stopColor="rgba(253,101,0,1)" />
-                           <stop offset="30%" stopColor="rgba(0,254,212,1)" />
-                        </radialGradient>
-                     </defs>
-
-                     <path
-                        fill="url(#myGradient)"
-                        d="M15 12c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-.199.02-.393.057-.581 1.474.541 2.927-.882 2.405-2.371.174-.03.354-.048.538-.048 1.657 0 3 1.344 3 3zm-2.985-7c-7.569 0-12.015 6.551-12.015 6.551s4.835 7.449 12.015 7.449c7.733 0 11.985-7.449 11.985-7.449s-4.291-6.551-11.985-6.551zm-.015 12c-2.761 0-5-2.238-5-5 0-2.761 2.239-5 5-5 2.762 0 5 2.239 5 5 0 2.762-2.238 5-5 5z"
-                     />
-                  </svg>
                </div>
 
                {/* Desktop Navigation */}
-               <nav className="hidden md:flex items-center gap-8 ml-4">
+               <nav className=" hidden md:flex items-center gap-8 ml-4">
                   <button
                      onClick={() => scrollToSection('home')}
-                     className="text-pink hover:text-orange transition-colors cursor-pointer"
+                     className=" transition-colors cursor-pointer text-lg hover:text-blue"
                   >
                      Accueil
                   </button>
                   <button
                      onClick={() => scrollToSection('about')}
-                     className="text-pink hover:text-orange transition-colors cursor-pointer"
+                     className=" transition-colors cursor-pointer text-lg hover:text-blue"
                   >
                      À propos
                   </button>
                   <button
                      onClick={() => scrollToSection('projects')}
-                     className="text-pink hover:text-orange transition-colors cursor-pointer"
+                     className=" transition-colors cursor-pointer text-lg hover:text-blue"
                   >
                      Projets
                   </button>
                   <button
                      onClick={() => scrollToSection('skills')}
-                     className="text-pink hover:text-orange transition-colors cursor-pointer"
+                     className=" transition-colors cursor-pointer text-lg hover:text-blue"
                   >
                      Compétences
                   </button>
                   <button
                      onClick={() => scrollToSection('contact')}
-                     className="px-4 py-2  text-pink rounded-lg hover:text-orange transition-colors cursor-pointer"
+                     className=" transition-colors cursor-pointer text-lg hover:text-blue"
                   >
                      Contact
                   </button>
@@ -114,9 +66,9 @@ export default function Header() {
                   aria-label="Toggle mobile menu"
                >
                   {mobileMenuOpen ? (
-                     <X className="text-pink" size={24} />
+                     <X className="text-blue" size={30} />
                   ) : (
-                     <Menu className="text-pink" size={24} />
+                     <Menu className="text-blue" size={30} />
                   )}
                </button>
             </div>
@@ -124,35 +76,35 @@ export default function Header() {
 
          {/* Mobile Navigation */}
          {mobileMenuOpen && (
-            <div className="md:hidden bg-black border-b border-yellow">
-               <nav className="container mx-auto px-4 py-4 flex flex-col gap-4">
+            <div className="md:hidden border-b border-yellow">
+               <nav className="container border-t-2 border-blue mx-auto px-4 py-4 flex flex-col gap-4">
                   <button
                      onClick={() => scrollToSection('home')}
-                     className="text-left text-pink hover:text-orange transition-colors"
+                     className="text-left  transition-colors"
                   >
                      Accueil
                   </button>
                   <button
                      onClick={() => scrollToSection('about')}
-                     className="text-left text-pink hover:text-orange transition-colors"
+                     className="text-left  transition-colors"
                   >
                      À propos
                   </button>
                   <button
                      onClick={() => scrollToSection('projects')}
-                     className="text-left text-pink hover:text-orange transition-colors"
+                     className="text-left  transition-colors"
                   >
                      Projets
                   </button>
                   <button
                      onClick={() => scrollToSection('skills')}
-                     className="text-left text-pink hover:text-orange transition-colors"
+                     className="text-left  transition-colors"
                   >
                      Compétences
                   </button>
                   <button
                      onClick={() => scrollToSection('contact')}
-                     className="text-left text-pink hover:text-orange transition-colors"
+                     className="text-left  transition-colors"
                   >
                      Contact
                   </button>
--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -12,72 +12,20 @@ export default function Intro() {
    return (
       <section
          id="home"
-         className="min-h-screen bg-black flex items-center justify-center pt-16 px-4 sm:px-6 lg:px-8 mt-8"
+         className="min-h-screen bg-cover bg-center bg-[url('/background.jpg')] flex items-center justify-center pt-16 px-4 sm:px-6 lg:px-8 mt-8"
       >
          <div className="container mx-auto">
             <div className="max-w-4xl mx-auto text-center">
-               <div className="mb-6 flex gap-5 md:gap-8 mx-auto w-fit">
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     width="24"
-                     height="24"
-                     viewBox="0 0 24 24"
-                     className="size-25 md:size-40"
-                  >
-                     <defs>
-                        <radialGradient
-                           id="myGradient"
-                           cx="50%"
-                           cy="50%"
-                           r="70%"
-                        >
-                           <stop offset="30%" stopColor="rgba(253,101,0,1)" />
-                           <stop offset="30%" stopColor="rgba(0,254,212,1)" />
-                        </radialGradient>
-                     </defs>
-
-                     <path
-                        fill="url(#myGradient)"
-                        d="M15 12c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-.199.02-.393.057-.581 1.474.541 2.927-.882 2.405-2.371.174-.03.354-.048.538-.048 1.657 0 3 1.344 3 3zm-2.985-7c-7.569 0-12.015 6.551-12.015 6.551s4.835 7.449 12.015 7.449c7.733 0 11.985-7.449 11.985-7.449s-4.291-6.551-11.985-6.551zm-.015 12c-2.761 0-5-2.238-5-5 0-2.761 2.239-5 5-5 2.762 0 5 2.239 5 5 0 2.762-2.238 5-5 5z"
-                     />
-                  </svg>
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     width="24"
-                     height="24"
-                     viewBox="0 0 24 24"
-                     className="size-25 md:size-40"
-                  >
-                     <defs>
-                        <radialGradient
-                           id="myGradient"
-                           cx="50%"
-                           cy="50%"
-                           r="70%"
-                        >
-                           <stop offset="30%" stopColor="rgba(253,101,0,1)" />
-                           <stop offset="30%" stopColor="rgba(0,254,212,1)" />
-                        </radialGradient>
-                     </defs>
-
-                     <path
-                        fill="url(#myGradient)"
-                        d="M15 12c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-.199.02-.393.057-.581 1.474.541 2.927-.882 2.405-2.371.174-.03.354-.048.538-.048 1.657 0 3 1.344 3 3zm-2.985-7c-7.569 0-12.015 6.551-12.015 6.551s4.835 7.449 12.015 7.449c7.733 0 11.985-7.449 11.985-7.449s-4.291-6.551-11.985-6.551zm-.015 12c-2.761 0-5-2.238-5-5 0-2.761 2.239-5 5-5 2.762 0 5 2.239 5 5 0 2.762-2.238 5-5 5z"
-                     />
-                  </svg>
-               </div>
-
-               <h1 className="text-yellow text-4xl sm:text-5xl lg:text-6xl mb-6">
+               <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl mb-6">
                   Un projet, une idée ?<br />
-                  <span className="text-orange">Clément </span>
-                  <span className="text-turquoise">Développe</span>
+                  On le{' '}
+                  <span className="squarified text-6xl md:text-7xl lg:text-8xl">
+                     D-vlop
+                  </span>{' '}
+                  ensemble
                </h1>
 
-               <h2 className="text-green text-2xl sm:text-3xl lg:text-4xl text-text1 mb-8">
-                  Développeur Web
-               </h2>
-
-               <p className="text-lg text-yellow mb-10 max-w-2xl mx-auto">
+               <p className="text-lg text-white mb-10 max-w-2xl mx-auto">
                   Je conçois des expériences numériques modernes, performantes
                   et accessibles. Avec une passion pour le code propre et les
                   interfaces intuitives, je transforme les idées en réalité
@@ -87,13 +35,13 @@ export default function Intro() {
                <div className="flex flex-wrap items-center justify-center gap-4 mb-12">
                   <button
                      onClick={() => scrollToSection('projects')}
-                     className="border-1 border-pink px-8 py-3 bg-primary text-pink  hover:text-orange hover:border-orange transition-colors"
+                     className="border border-blue px-8 py-3 bg-white/60 text-blue  hover:text-white hover:bg-blue transition-colors rounded-full font-semibold shadow-lg"
                   >
                      Voir mes projets
                   </button>
                   <button
                      onClick={() => scrollToSection('contact')}
-                     className="px-8 py-3 border-1 border-pink bg-secondary text-pink  hover:text-orange hover:border-orange transition-colors"
+                     className="border border-blue px-8 py-3 bg-white/60 text-blue  hover:text-white hover:bg-blue transition-colors rounded-full font-semibold shadow-lg"
                   >
                      Contactez-moi
                   </button>
